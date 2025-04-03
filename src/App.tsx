c5e03d4adba09cb664c052915496d57762ba696f
Badri467 <114822924+Badri467@users.noreply.github.com>
2025-04-03 10:26:39 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  {Navbar}  from "./components/Navbar";
+import { TimedCards } from "./components/timed-cards";
+import { MobileView } from "./components/MobileView";
+import { Team } from "./pages/Team";
+import { Events } from "./pages/Events";
+import { useDeviceDetect } from "./useDeviceDetect";
+import Main from "./components/Main";
+import RegistrationForm from "./components/RegistrationForm";
+
+
+function App() {
+  const isMobile = useDeviceDetect();
+
+  return (
+    <Router>
+      <div className="min-h-screen bg-[#1a1a1a] text-[#FFFFFFDD]">
+        <Navbar />
+        <Routes>
+          {/* Detect device and render the appropriate view */}
+          <Route path="/" element={<Main/>} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/events" element={isMobile? <MobileView/>:<TimedCards />} />
+          <Route path="/register" element={<RegistrationForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
